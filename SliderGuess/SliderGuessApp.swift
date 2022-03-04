eea5242e8255050520b80cdb9d6e992e1e7cb6d5
Snow Lukin <everyoursden@gmail.com>
2022-03-04 14:50:27 +0300

    Initial Commit

---
+++ SliderGuess/SliderGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGuessApp.swift
+//  SliderGuess
+//
+//  Created by Snow Lukin on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
