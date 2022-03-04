c60b6ebc33368aba1a0c0183eb4feb43636e6b4d
Snow Lukin <everyoursden@gmail.com>
2022-03-04 15:31:10 +0300

    Added main logic.

--- SliderGuess/ContentView.swift
+++ SliderGuess/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var targetValue = Int.random(in: 1...100)
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+        
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            GameSlider(
+                currentValue: $currentValue,
+                targetValue: targetValue,
+                color: .red,
+                alpha: computeScore()
+            )
+            
+            Button("Check") {
+                showAlert = true
+            }
             .padding()
+            .alert("Your Score", isPresented: $showAlert, actions: {}) {
+                Text("\(computeScore())")
+            }
+            
+            Button("Change") {
+                targetValue = Int.random(in: 1...100)
+            }
+        }
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
---
+++ SliderGuess/Views/GameSlider.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SliderGuess
+//
+//  Created by Snow Lukin on 04.03.2022.
+//
+
+import SwiftUI
+
+struct GameSlider: View {
+    @Binding var currentValue: Double
+    
+    let targetValue: Int
+    let color: UIColor
+    let alpha: Int
+    
+    var body: some View {
+        VStack {
+            Text("Goal: \(targetValue)")
+            HStack {
+                Text("0")
+                CustomSlider(value: $currentValue, alpha: alpha, color: color)
+                Text("100")
+            }
+            .padding()
+        }
+    }
+}
+
+struct GameSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        GameSlider(
+            currentValue: .constant(50),
+            targetValue: 100,
+            color: .red,
+            alpha: 100
+        )
+    }
+}
